d2293b2c7cca1600949c517b39ebb1cbe846a504
Yugam <63348402+yugam08@users.noreply.github.com>
2020-04-25 11:27:13 +0530

    Update code.py

--- Project-Student-Management-System/code.py
+++ Project-Student-Management-System/code.py
@@ -28,16 +28,12 @@ print(percentage)
 mathematics = {'Geoffrey Hinton':78,'Andrew Ng':95,'Sebastian Raschka':65,'Yoshua Benjio':50,'Hilary Mason':70,'Corinna Cortes':66,'Peter Warden':75}
 topper  = max(mathematics,key = mathematics.get)
 print(topper)
-
-
 # Code ends here  
 
 
 # --------------
 # Given string
 topper = 'andrew ng'
-
-
 # Code starts here
 first_name, last_name = topper.split(" ")
 full_name = last_name + ' ' + first_name
