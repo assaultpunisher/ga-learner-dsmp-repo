b543c8a6e9fb884185ce1f451421a433e586deed
Yugam <63348402+yugam08@users.noreply.github.com>
2020-05-17 15:08:58 +0530

    Update code.py

--- Project-Loan-Approval-Analysis/code.py
+++ Project-Loan-Approval-Analysis/code.py
@@ -1,20 +1,34 @@
 # --------------
 # Import packages
+
 import numpy as np
 import pandas as pd
 from scipy.stats import mode 
- 
+
+
 
 # code starts here
+#Load Dataset
 
 bank = pd.read_csv(path)
-print(bank.head())
+    
+# Display categorical variable
 
-categorical_var = bank.select_dtypes(include='object')
-print(categorical_var.head())
 
-numerical_var = bank.select_dtypes(include = 'number')
-print(numerical_var.head())
+categorical_var=bank.select_dtypes(include='object')
+
+
+#print("Categorical variables : ",categorical_var)
+
+
+    
+#Code for numerical variable
+
+numerical_var=bank.select_dtypes(include='number')
+
+#print("Numerical Variables : ",numerical_var)
+
+
 
 # code ends here
 
@@ -22,24 +36,23 @@ print(numerical_var.head())
 # --------------
 # code starts here
 
-banks = bank.drop(['Loan_ID'],axis=1)
+# load the dataset and drop the Loan_ID
+banks= bank.drop(columns='Loan_ID')
+
+
+# check  all the missing values filled.
 
 print(banks.isnull().sum())
 
-bank_mode = banks.mode()
+# apply mode 
 
-banks['Gender'].fillna(bank_mode['Gender'][0],inplace=True)
-banks['Married'].fillna(bank_mode['Married'][0],inplace=True)
-banks['Dependents'].fillna(bank_mode['Dependents'][0],inplace=True)
-banks['Education'].fillna(bank_mode['Education'][0],inplace=True)
-banks['Self_Employed'].fillna(bank_mode['Self_Employed'][0],inplace=True)
-banks['ApplicantIncome'].fillna(bank_mode['ApplicantIncome'][0],inplace=True)
-banks['CoapplicantIncome'].fillna(bank_mode['CoapplicantIncome'][0],inplace=True)
-banks['LoanAmount'].fillna(bank_mode['LoanAmount'][0],inplace=True)
-banks['Loan_Amount_Term'].fillna(bank_mode['Loan_Amount_Term'][0],inplace=True)
-banks['Credit_History'].fillna(bank_mode['Credit_History'][0],inplace=True)
-banks['Property_Area'].fillna(bank_mode['Property_Area'][0],inplace=True)
-banks['Loan_Status'].fillna(bank_mode['Loan_Status'][0],inplace=True)
+bank_mode = banks.mode().iloc[0]
+
+# Fill the missing values with 
+
+banks.fillna(bank_mode, inplace=True)
+
+# check again all the missing values filled.
 
 print(banks.isnull().sum())
 
@@ -49,9 +62,10 @@ print(banks.isnull().sum())
 # --------------
 # Code starts here
 
-avg_loan_amount = pd.pivot_table(banks,index=['Gender', 'Married', 'Self_Employed'],
-                                values='LoanAmount',aggfunc='mean')
-print(avg_loan_amount)
+# check the avg_loan_amount
+avg_loan_amount = banks.pivot_table(values=["LoanAmount"], index=["Gender","Married","Self_Employed"], aggfunc=np.mean)
+
+print (avg_loan_amount)
 
 # code ends here
 
@@ -60,17 +74,25 @@ print(avg_loan_amount)
 # --------------
 # code starts here
 
-loan_approved_se = banks[(banks['Self_Employed']=='Yes') & (banks['Loan_Status']=='Y')]
+# code for loan aprroved for self employed
+loan_approved_se = banks.loc[(banks["Self_Employed"]=="Yes")  & (banks["Loan_Status"]=="Y"), ["Loan_Status"]].count()
+print(loan_approved_se)
 
-loan_approved_nse = banks[(banks['Self_Employed']=='No') & (banks['Loan_Status']=='Y')]
+# code for loan approved for non self employed
+loan_approved_nse = banks.loc[(banks["Self_Employed"]=="No")  & (banks["Loan_Status"]=="Y"), ["Loan_Status"]].count()
+print(loan_approved_nse)
 
-Loan_Status = 614
-
-percentage_se = (len(loan_approved_se)/Loan_Status)*100
+# percentage of loan approved for self employed
+percentage_se = (loan_approved_se * 100 / 614)
+percentage_se=percentage_se[0]
+# print percentage of loan approved for self employed
 print(percentage_se)
 
-percentage_nse = (len(loan_approved_nse))*100/Loan_Status
-print(percentage_nse)
+#percentage of loan for non self employed
+percentage_nse = (loan_approved_nse * 100 / 614)
+percentage_nse=percentage_nse[0]
+#print percentage of loan for non self employed
+print (percentage_nse)
 
 # code ends here
 
@@ -78,10 +100,12 @@ print(percentage_nse)
 # --------------
 # code starts here
 
-loan_term  = banks['Loan_Amount_Term'].apply(lambda x: x/12)
-print(loan_term.head())
+# loan amount term 
 
-big_loan_term = len(banks[banks['Loan_Amount_Term']>=25*12])
+loan_term = banks['Loan_Amount_Term'].apply(lambda x: int(x)/12 )
+
+big_loan_term=len(loan_term[loan_term>=25])
+
 print(big_loan_term)
 
 # code ends here
@@ -90,11 +114,15 @@ print(big_loan_term)
 # --------------
 # code starts here
 
-loan_groupby = banks.groupby('Loan_Status')
+columns_to_show = ['ApplicantIncome', 'Credit_History']
+ 
+loan_groupby=banks.groupby(['Loan_Status'])
 
-loan_groupby = loan_groupby[['ApplicantIncome', 'Credit_History']]
+loan_groupby=loan_groupby[columns_to_show]
 
-mean_values = loan_groupby.mean()
+# Check the mean value 
+mean_values=loan_groupby.agg([np.mean])
+
 print(mean_values)
 
 # code ends here
